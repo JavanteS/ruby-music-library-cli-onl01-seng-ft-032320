c6e2d896e6bd06bfc7df593eb3b069cc8a738813
Javante Nelson <javantenelson@gmail.com>
2020-04-11 20:14:45 +0000

    Automatically backed up by Learn

---
+++ lib/musiclibrarycontroller.rb
@@ -0,0 +1,68 @@
+class MusicLibraryController
+  
+      attr_accessor :path 
+      
+  def initialize(path=("./db/mp3s"))
+    @path = path 
+    songo = MusicImporter.new(path)
+    new_songo = songo.import 
+  end 
+  
+  def call
+    puts "Welcome to your music library!"
+    user_input = gets.chomp
+    puts "To list all of your songs, enter 'list songs'."
+    puts "To list all of the artists in your library, enter 'list artists'."
+    puts "To list all of the genres in your library, enter 'list genres'."
+    puts "To list all of the songs by a particular artist, enter 'list artist'."
+    puts "To list all of the songs of a particular genre, enter 'list genre'."
+    puts "To play a song, enter 'play song'."
+    puts "To quit, type 'exit'."
+    puts "What would you like to do?"
+    if user_input != "exit"
+      call 
+    else 
+    end 
+  end 
+    
+    def list_songs
+      #binding.pry
+      Song.all.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |a, i|
+      puts "#{i}. #{a.artist.name} - #{a.name} - #{a.genre.name}"
+      end
+    end 
+    
+    def list_artists
+      #binding.pry
+      Artist.all.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |a,i|
+      puts "#{i}. #{a.name}"
+      end
+    end
+
+      
+    
+    
+     def list_genres
+      #binding.pry
+      #sorted_name = Song.all.sort_by { |song| song.genre.name }
+      #sorted_name.each_with_index { |song, index| puts "#{index + 1}. #{song.genre.name}"}
+      
+      Genre.all.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |a,i|
+      puts "#{i}. #{a.name}"
+      end
+    end 
+    
+    def list_songs_by_genre
+    end
+    
+    def list_songs_by_artist
+      puts "Please enter the name of an artist:"
+      user_input = gets.chomp
+      
+    end 
+    
+    def play_song
+    end 
+  
+  
+end 
