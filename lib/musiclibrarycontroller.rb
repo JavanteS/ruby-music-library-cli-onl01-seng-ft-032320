d77291a9acbe7f57744afd0861a3995009203d80
Javante Nelson <javantenelson@gmail.com>
2020-04-11 20:32:00 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -59,7 +59,7 @@ class MusicLibraryController
         genre_sorted = genre.songs.sort_by do |song| song.name 
       end 
         genre_sorted.each.with_index(1) do |song, index| 
-          puts "#{index}."
+          puts "#{index}. #{song.artist.name} - #{song.name}"
         end 
       end 
     end
