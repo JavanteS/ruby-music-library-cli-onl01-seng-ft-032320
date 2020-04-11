62fac93a8f04be98787d59d146ed241fff47a985
Javante Nelson <javantenelson@gmail.com>
2020-04-11 20:22:12 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -63,7 +63,7 @@ class MusicLibraryController
         
         sosinger_sorted = singer.songs.sort_by do |song| song.name 
       end 
-      sosinger_sorted.each_with_index(1) do |artist,index|
+      sosinger_sorted.each.with_index(1) do |artist,index|
         puts "#{i}"
       end
     end
