db990715dad9a9aed16dc5189ae1b95b04d7f2e5
Javante Nelson <javantenelson@gmail.com>
2020-04-11 20:27:33 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -55,7 +55,7 @@ class MusicLibraryController
     def list_songs_by_genre
       puts "Please enter the name of an artist:"
       user_input = gets.chomp
-      puts "Please enter the name of a genre:"
+      
     end
     
     def list_songs_by_artist
