3df2267a01f434fe37947a0aaf401450808a358c
Javante Nelson <javantenelson@gmail.com>
2020-04-11 21:41:27 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -11,6 +11,7 @@ class MusicLibraryController
   def call
     puts "Welcome to your music library!"
     user_input = gets.chomp
+    While user_input != exit 
     puts "To list all of your songs, enter 'list songs'."
     puts "To list all of the artists in your library, enter 'list artists'."
     puts "To list all of the genres in your library, enter 'list genres'."
@@ -19,10 +20,7 @@ class MusicLibraryController
     puts "To play a song, enter 'play song'."
     puts "To quit, type 'exit'."
     puts "What would you like to do?"
-    if user_input != "exit"
-      call 
-    elsif user_input == "list_songs"
-      list_songs
+    
     end 
   end 
     
